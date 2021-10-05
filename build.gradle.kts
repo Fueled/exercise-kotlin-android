07dd372c9e0b8bf65b780b2663802d207fcfb44b
Anvith Bhat <1428864+humblerookie@users.noreply.github.com>
2021-10-05 10:39:24 +0530

    refactor(gradle): upgrade gradle (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    var kotlin_version: String by extra
-    kotlin_version = "1.2.31"
-
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath(kotlinModule("gradle-plugin", kotlin_version))
-    }
+plugins {
+    kotlin("jvm") version "1.5.31"
 }
 
 group = "com.fueled"
 version = "1.0-SNAPSHOT"
-
-apply {
-    plugin("kotlin")
-}
-
-val kotlin_version: String by extra
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlinModule("stdlib-jdk8", kotlin_version))
-    compile("com.google.code.gson:gson:2.8.6")
+    testImplementation(kotlin("test"))
+    implementation("com.google.code.gson:gson:2.8.6")
 }
 
-tasks.withType<KotlinCompile> {
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
